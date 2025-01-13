0a92ac69068ea82adf47e0405609a6473b5eabd4
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-14 00:06:56 +0530

    loops added

--- src/com/apnacollege/loops.java
+++ src/com/apnacollege/loops.java
@@ -6,6 +6,10 @@ public class loops  {
         for(int i = 0; i <= 10; i++){
             System.out.println(i);
         }
+        for(int j = 100; j>=1; j--){
+            System.out.println(j);
+        }
+
     }
 
 }
