f465ddb72202e40b711b31afe4097ea6f7fb9b16
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-14 00:43:15 +0530

    Methods added

---
+++ src/com/apnacollege/Methods.java
@@ -0,0 +1,13 @@
+package com.apnacollege;
+
+public class Methods{
+    public static void printJava(){
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args){
+        // Methods or functions
+          printJava();
+    }
+
+}
