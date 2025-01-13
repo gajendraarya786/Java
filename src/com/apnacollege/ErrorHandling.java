2d8ba987196a5424f4f5cdfe8037fdad57708917
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-14 00:27:48 +0530

    Error handling

---
+++ src/com/apnacollege/ErrorHandling.java
@@ -0,0 +1,16 @@
+package com.apnacollege;
+
+public class ErrorHandling{
+    public static void main(String[] args){
+        //TRY - CATCH in Exception handling
+        int[] marks = {97, 98, 95};
+        System.out.println("The name is Aman");
+        try{
+            System.out.println(marks[5]);
+        }
+        catch(Exception exception){
+            //do something
+        }
+        }
+    }
+
